989b0fa9b4f53bc687b4373ee07f4fc03a9ea62b
Dmytro_Bystrov <bystrovdmytro1106@gmail.com>
2024-06-12 00:58:19 +0300

    feat(Player) Basic instance

--- Source/DBystrov_BGS_Task/Private/Character/BG_PlayerCharacter.cpp
+++ Source/DBystrov_BGS_Task/Private/Character/BG_PlayerCharacter.cpp
@@ -3,32 +3,161 @@
 
 #include "Character/BG_PlayerCharacter.h"
 
+#include "InputAction.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+#include "Camera/CameraComponent.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+#include "BG_SpringArmComponent.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
 ABG_PlayerCharacter::ABG_PlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//..............................Set components.................................//
+
+	SpringArmComponent = CreateDefaultSubobject<UBG_SpringArmComponent>("CameraSystem");
+	SpringArmComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform);
+	SpringArmComponent->SocketOffset = FVector(0, 100, 50);
+	SpringArmComponent->SetRelativeLocation(FVector(0, 0, 100));
+	SpringArmComponent->bUsePawnControlRotation = false;
+
+	ThirdPersonCamera = CreateDefaultSubobject<UCameraComponent>("ThirdPersonCamera");
+	ThirdPersonCamera->AttachToComponent(SpringArmComponent, FAttachmentTransformRules::KeepRelativeTransform, USpringArmComponent::SocketName);
+	ThirdPersonCamera->bUsePawnControlRotation = false;
+	ThirdPersonCamera->SetActive(true);
 }
 
-// Called when the game starts or when spawned
+
 void ABG_PlayerCharacter::BeginPlay()
-{
+{	
 	Super::BeginPlay();
-	
+
+	GetCharacterMovement()->BrakingFriction = BrakingFrictionValue;
+	GetCharacterMovement()->MaxWalkSpeed = GlideSpeed;
 }
 
-// Called every frame
+
+
 void ABG_PlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	AlignToSurface();
 }
 
-// Called to bind functionality to input
+
+
 void ABG_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		//................Moving................//
+
+		EnhancedInputComponent->BindAction(GetActionByName("Move"), ETriggerEvent::Triggered, this, &ThisClass::Move);
+		EnhancedInputComponent->BindAction(GetActionByName("EmergencyStop"), ETriggerEvent::Triggered, this, &ThisClass::EmergencyStop);
+
+		EnhancedInputComponent->BindAction(GetActionByName("Look"), ETriggerEvent::Triggered, this, & ThisClass::Look);
+
+		EnhancedInputComponent->BindAction(GetActionByName("Jump"), ETriggerEvent::Triggered, this, &ThisClass::OnJumpPressed);
+
+		EnhancedInputComponent->BindAction(GetActionByName("SprintStart"), ETriggerEvent::Triggered, this, &ThisClass::OnPressSprint);
+		EnhancedInputComponent->BindAction(GetActionByName("SprintEnd"), ETriggerEvent::Triggered, this, &ThisClass::OnReleaseSprint);
+
+		ULocalPlayer* LLocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+		auto EnhancedInputSubsystem = LLocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LLocalPlayer);
+		if (IsValid(EnhancedInputSubsystem)) 
+		{
+			EnhancedInputSubsystem->AddMappingContext(Context, 0);
+		}
+	}
+
 }
 
+void ABG_PlayerCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		AddControllerPitchInput(LookAxisVector.Y);
+		AddControllerYawInput(LookAxisVector.X);
+	}
+}
+
+void ABG_PlayerCharacter::OnJumpPressed()
+{
+	Jump();
+}
+
+
+UInputAction* ABG_PlayerCharacter::GetActionByName(const FName ActionName)
+{
+	auto ActionLink = InputActions.Find(ActionName);
+	check(ActionLink);
+
+	return *ActionLink;
+}
+
+void ABG_PlayerCharacter::ChangeMoveType_Implementation(EMoveType NewMoveType)
+{
+	MoveType = NewMoveType;
+
+	OnMoveTypeChanged(NewMoveType);
+	OnMoveTypeChangedBind.Broadcast(NewMoveType);
+}
+
+void ABG_PlayerCharacter::OnPressSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+	GetCharacterMovement()->BrakingFriction = GlidingFrictionValue;
+	ChangeMoveType(EMoveType::Sprint);
+}
+
+void ABG_PlayerCharacter::OnReleaseSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = GlideSpeed;
+	ChangeMoveType(EMoveType::Glide);
+}
+
+
+void ABG_PlayerCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	FRotator LookRotate = FRotator(0, GetControlRotation().Yaw, 0);
+
+	AddMovementInput(FRotationMatrix(LookRotate).GetUnitAxis(EAxis::X), MovementVector.Y);
+}
+
+void ABG_PlayerCharacter::EmergencyStop()
+{
+	GetCharacterMovement()->BrakingFriction = BrakingFrictionValue;
+}
+
+void ABG_PlayerCharacter::AlignToSurface()
+{
+	FVector Start = GetActorLocation();
+	FVector End = Start - FVector(0, 0, 1000.0f);
+
+	FHitResult HitResult;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility, Params))
+	{
+		FVector SurfaceNormal = HitResult.Normal;
+		FRotator NewRotation = UKismetMathLibrary::MakeRotFromXZ(GetActorForwardVector(), SurfaceNormal);
+		SetActorRotation(NewRotation);
+	}
+}
--- Source/DBystrov_BGS_Task/Public/Character/BG_PlayerCharacter.h
+++ Source/DBystrov_BGS_Task/Public/Character/BG_PlayerCharacter.h
@@ -3,8 +3,24 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
 #include "GameFramework/Character.h"
 #include "BG_PlayerCharacter.generated.h"
+
+
+
+UENUM(BlueprintType)
+enum class EMoveType : uint8
+{
+	Glide		UMETA(DisplayName = "Glide"),
+	Sprint		UMETA(DisplayName = "Sprint")
+};
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMoveTypeChanged, EMoveType, NewMoveType);
+
 
 UCLASS()
 class DBYSTROV_BGS_TASK_API ABG_PlayerCharacter : public ACharacter
@@ -12,18 +28,86 @@ class DBYSTROV_BGS_TASK_API ABG_PlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	ABG_PlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void Look(const FInputActionValue& Value);
+
+	void Move(const FInputActionValue& Value);
+	void EmergencyStop();
+	void OnJumpPressed();
+	void OnPressSprint();
+	void OnReleaseSprint();
+
+
+	void AlignToSurface();
+
+
+//c++ private values
+private:
+
+	UPROPERTY()
+	EMoveType MoveType;
+
+//c++ public values
+private:
+
+
+
+//Blueprint methods
+public:
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		EMoveType GetMoveType() { return MoveType; };
+
+	UFUNCTION(BlueprintCallable)
+		UInputAction* GetActionByName(const FName ActionName);
+
+	UFUNCTION(BlueprintCallable, Server, Reliable, Category = "BaseCharacter|Moving")
+		void ChangeMoveType(EMoveType NewLegMoveType);
+		void ChangeMoveType_Implementation(EMoveType NewLegMoveType);
+	UPROPERTY(BlueprintAssignable, Category = "BaseCharacter|EventsForBind")
+		FOnMoveTypeChanged OnMoveTypeChangedBind;
+
+
+	UFUNCTION(BlueprintNativeEvent, meta = (BlueprintProtected), Category = "BaseCharacter")
+		void OnMoveTypeChanged(EMoveType NewLegMoveTyp);
+		virtual void OnMoveTypeChanged_Implementation(EMoveType NewLegMoveTyp) { }
+
+//Bluepring values
+public:
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerCharacter|Components")
+		class UBG_SpringArmComponent* SpringArmComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerCharacter|Components")
+		class UCameraComponent* ThirdPersonCamera = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+		TMap<FName, UInputAction*> InputActions;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (ClampMin = "0.0", UIMin = "0.0"), Category = "PlayerCharacter")
+		float GlideSpeed = 400.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (ClampMin = "0.0", UIMin = "0.0"), Category = "PlayerCharacter")
+		float SprintSpeed = 800.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (ClampMin = "0.0", UIMin = "0.0"), Category = "PlayerCharacter")
+		float GlidingFrictionValue = 10.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (ClampMin = "0.0", UIMin = "0.0"), Category = "PlayerCharacter")
+		float BrakingFrictionValue = 200.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+		UInputMappingContext* Context = nullptr;
 };
